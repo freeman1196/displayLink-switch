fe9af80b5d89ed66f0d66acf46e1fb418df4ef36
freeman1196 <aza.haydarov11@gmail.com>
2023-11-17 14:26:19 +0500

    relase v1

---
+++ js/script.js
@@ -0,0 +1,167 @@
+const daysContainer = document.getElementById("daysContainer");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const monthYear = document.getElementById("monthYear");
+const dateInput = document.getElementById("dateInput");
+const calendar = document.getElementById("calendar");
+
+let currentDate = new Date();
+let selectedDate = null;
+
+function handleDayClick(day) {
+  selectedDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    day
+  );
+  dateInput.value = selectedDate.toLocaleDateString("en-US");
+  calendar.style.display = "none";
+  renderCalendar();
+}
+
+function createDayElement(day) {
+  const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), day);
+  const dayElement = document.createElement("div");
+  dayElement.classList.add("day");
+
+  if (date.toDateString() === new Date().toDateString()) {
+    dayElement.classList.add("current");
+  }
+  if (selectedDate && date.toDateString() === selectedDate.toDateString()) {
+    dayElement.classList.add("selected");
+  }
+
+  dayElement.textContent = day;
+  dayElement.addEventListener("click", () => {
+    handleDayClick(day);
+  });
+  daysContainer.appendChild(dayElement);
+}
+
+function renderCalendar() {
+  daysContainer.innerHTML = "";
+  const firstDay = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+  const lastDay = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  );
+
+  monthYear.textContent = `${currentDate.toLocaleString("default", {
+    month: "long"
+  })} ${currentDate.getFullYear()}`;
+
+  for (let day = 1; day <= lastDay.getDate(); day++) {
+    createDayElement(day);
+  }
+}
+
+prevBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalendar();
+    positionCalendar();
+  });
+  
+  nextBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    renderCalendar();
+    positionCalendar();
+  });
+
+dateInput.addEventListener("click", () => {
+  calendar.style.display = "block";
+  positionCalendar();
+});
+
+document.addEventListener("click", (event) => {
+  if (!dateInput.contains(event.target) && !calendar.contains(event.target)) {
+    calendar.style.display = "none";
+  }
+});
+
+function positionCalendar() {
+  const inputRect = dateInput.getBoundingClientRect();
+  calendar.style.top = inputRect.bottom + "px";
+  calendar.style.left = inputRect.left + "px";
+}
+
+window.addEventListener("resize", positionCalendar);
+
+renderCalendar();
+
+
+
+
+
+
+
+// Iterate over each select element
+$('select').each(function () {
+
+    // Cache the number of options
+    var $this = $(this),
+        numberOfOptions = $(this).children('option').length;
+
+    // Hides the select element
+    $this.addClass('s-hidden');
+
+    // Wrap the select element in a div
+    $this.wrap('<div class="select"></div>');
+
+    // Insert a styled div to sit over the top of the hidden select element
+    $this.after('<div class="styledSelect"></div>');
+
+    // Cache the styled div
+    var $styledSelect = $this.next('div.styledSelect');
+
+    // Show the first select option in the styled div
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    // Insert an unordered list after the styled div and also cache the list
+    var $list = $('<ul />', {
+        'class': 'options'
+    }).insertAfter($styledSelect);
+
+    // Insert a list item into the unordered list for each select option
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+
+    // Cache the list items
+    var $listItems = $list.children('li');
+
+    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+    $styledSelect.click(function (e) {
+        e.stopPropagation();
+        $('div.styledSelect.active').each(function () {
+            $(this).removeClass('active').next('ul.options').hide();
+        });
+        $(this).toggleClass('active').next('ul.options').toggle();
+    });
+
+    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+    // Updates the select element to have the value of the equivalent option
+    $listItems.click(function (e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        /* alert($this.val()); Uncomment this for demonstration! */
+    });
+
+    // Hides the unordered list when clicking outside of it
+    $(document).click(function () {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
